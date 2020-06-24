5e1b3ff22c1eaa881c944c61e2a90a25a6690fb1
邓江洲 <707703386@qq.com>
2020-06-24 10:51:19 +0800

    easy 简化

--- CardView/Example/Pods/Target Support Files/MMCardView/MMCardView-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_MMCardView : NSObject
-@end
-@implementation PodsDummy_MMCardView
-@end
--- CardView/Example/Pods/Target Support Files/MMCardView/MMCardView-umbrella.h
+++
@@ -1,7 +0,0 @@
-//#import <UIKit/UIKit.h>
-#ifdef __OBJC__
-#import <UIKit/UIKit.h>
-#endif
-FOUNDATION_EXPORT double MMCardViewVersionNumber;
-FOUNDATION_EXPORT const unsigned char MMCardViewVersionString[];
-
